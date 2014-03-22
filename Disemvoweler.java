57c9e910eb6e516718e8875ef73fe6e62ec63730
jhn587 <jhn587@gmail.com>
2014-03-22 19:31:16 -0400

    Added capitals to vowel checker

--- Disemvoweler.java
+++ Disemvoweler.java
@@ -27,7 +27,7 @@ public class Disemvoweler{
 		for(int i = 0; i < original.length(); i++){
 			if( isAVowel(i)){
 				this.forVowels += this.original.charAt(i);
-			} else if( isASpace(i) ){
+			} else if( isASpaceOrPunctuation(i) ){
 				//Do Nothing
 			} else{//Adds punctuation to the cons string, maybe we fix this later
 				this.forCons += this.original.charAt(i);
@@ -40,11 +40,18 @@ public class Disemvoweler{
 		char toCheck = this.original.charAt(index);
 		//returns true if the character at the index is a vowel
 		switch(toCheck) {
+			//Lower case vowels
 			case 'a':
 			case 'e':
 			case 'i':
 			case 'o':
 			case 'u':
+			//Capital vowels
+			case 'A':
+			case 'E':
+			case 'I':
+			case 'O':
+			case 'U':
 				return true;
 				
 			default:
@@ -55,7 +62,7 @@ public class Disemvoweler{
 	}
 	//Returns true if the character at the index of the original string is a space
 	//Expand to punctuation
-	private boolean isASpace(int index){
+	private boolean isASpaceOrPunctuation(int index){
 		char toCheck = this.original.charAt(index);
 		return (toCheck == ' ')? true : false;
 	
--- Driver.java
+++ Driver.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Driver {
 	public static void main(String[] args) {
 		String testString1 = "Hi How Are You";
+		System.out.println("Enter a String to be disemvoweled");
+		Scanner keyInput = new Scanner(System.in);
+		
+		testString1 = keyInput.nextLine();
+		
 		Disemvoweler firstDis = new Disemvoweler(testString1);
+		
 		System.out.println(firstDis);
 	}
 
